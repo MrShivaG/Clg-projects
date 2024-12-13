194e9d2cdddcc419609a7e5917bb5b1283d59d3f
Shiva Ahirwar <106959325+MrShivaG@users.noreply.github.com>
2024-12-13 11:48:23 +0530

    Update

---
+++ pyrmid.c
@@ -0,0 +1,31 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n, spaces,stars;
+    printf("enter the number of lines you want: ");
+    scanf("%d",&n);
+   
+    spaces=n;
+    stars=1;
+    // Write C code here
+    for(int i=1;i<=n;i++){
+        for(int m=1;m<=spaces;m++){
+            printf(" ");
+        }
+        for(int o=1;o<=stars;o++){
+            printf("*");
+        }
+        for(int p=1;p<=spaces;p++){
+            printf(" ");
+            
+        }
+        spaces--;
+        stars = stars+2;
+        printf("\n");
+        
+        
+    }
+
+    return 0;
+}
